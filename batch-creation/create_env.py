55f33d72bf35d1d4fa488ad3bbeadb85aaaf1871
Pauline <ribeyre@uchicago.edu>
2020-10-26 10:55:35 -0500

    Add pre-commit config

--- batch-creation/create_env.py
+++ batch-creation/create_env.py
@@ -1,36 +1,36 @@
 import boto3
 
-client = boto3.client('batch')
+client = boto3.client("batch")
 
 response = client.update_compute_environment(
-    type='MANAGED',
-    computeEnvironmentName='M4Spot',
+    type="MANAGED",
+    computeEnvironmentName="M4Spot",
     computeResources={
-        'type': 'SPOT',
-        'bidPercentage': 20,
-        'desiredvCpus': 4,
-        'ec2KeyPair': 'id_rsa',
-        'instanceRole': 'ecsInstanceRole',
-        'instanceTypes': [
-            'm4',
+        "type": "SPOT",
+        "bidPercentage": 20,
+        "desiredvCpus": 4,
+        "ec2KeyPair": "id_rsa",
+        "instanceRole": "ecsInstanceRole",
+        "instanceTypes": [
+            "m4",
         ],
-        'maxvCpus': 128,
-        'minvCpus': 0,
-        'securityGroupIds': [
-            'sg-cf5093b2',
+        "maxvCpus": 128,
+        "minvCpus": 0,
+        "securityGroupIds": [
+            "sg-cf5093b2",
         ],
-        'spotIamFleetRole': 'arn:aws:iam::707767160287:role/aws-ec2-spot-fleet-role',
-        'subnets': [
-            'subnet-220c0e0a',
-            'subnet-1a95556d',
-            'subnet-978f6dce',
+        "spotIamFleetRole": "arn:aws:iam::707767160287:role/aws-ec2-spot-fleet-role",
+        "subnets": [
+            "subnet-220c0e0a",
+            "subnet-1a95556d",
+            "subnet-978f6dce",
         ],
-        'tags': {
-            'Name': 'Batch Instance - M4Spot',
+        "tags": {
+            "Name": "Batch Instance - M4Spot",
         },
     },
-    serviceRole='arn:aws:iam::707767160287:role/AWSBatchServiceRole',
-    state='ENABLED',
+    serviceRole="arn:aws:iam::707767160287:role/AWSBatchServiceRole",
+    state="ENABLED",
 )
 
 print response
--- batch-creation/md5_stream.py
+++ batch-creation/md5_stream.py
@@ -4,61 +4,75 @@ import sys
 import argparse
 import uuid
 
+
 def parse_arguments():
-	parser = argparse.ArgumentParser(description = "calculate the streaming md5 sum for s3 objects")
-	parser.add_argument('-b', '--bucketName', required=True, help='s3 bucket')
-	parser.add_argument('-o', '--objectName', required=True, help='object key in s3 bucket')
-	args = parser.parse_args()
+    parser = argparse.ArgumentParser(
+        description="calculate the streaming md5 sum for s3 objects"
+    )
+    parser.add_argument("-b", "--bucketName", required=True, help="s3 bucket")
+    parser.add_argument(
+        "-o", "--objectName", required=True, help="object key in s3 bucket"
+    )
+    args = parser.parse_args()
 
-	return args
+    return args
 
 
-s3 = boto3.resource('s3')
-client = boto3.client('s3')
+s3 = boto3.resource("s3")
+client = boto3.client("s3")
+
 
 def md5ObjectSum(bucketName, objectName):
-	bucket = s3.Bucket(bucketName)
-	obj = bucket.Object(objectName)
+    bucket = s3.Bucket(bucketName)
+    obj = bucket.Object(objectName)
 
-	size = obj.content_length
+    size = obj.content_length
 
-	hash_md5 = hashlib.md5()
+    hash_md5 = hashlib.md5()
 
-	step = 5000000
-	front = 0
-	back = 0 + step
-	while front != (size + 1):
-		byteRange = 'bytes=' + str(front) + '-' +str(back)
-		response = client.get_object(
-			Bucket = bucketName,
-			Key = objectName,
-			Range = byteRange
-		)
+    step = 5000000
+    front = 0
+    back = 0 + step
+    while front != (size + 1):
+        byteRange = "bytes=" + str(front) + "-" + str(back)
+        response = client.get_object(Bucket=bucketName, Key=objectName, Range=byteRange)
 
-		body = response['Body']
-		length  = response['ResponseMetadata']['HTTPHeaders']['content-length']
-		chunk = body.read()
-		hash_md5.update(chunk)
+        body = response["Body"]
+        length = response["ResponseMetadata"]["HTTPHeaders"]["content-length"]
+        chunk = body.read()
+        hash_md5.update(chunk)
 
-		front = back + 1
-		if (back + step) > size:
-			back = size
-		else:
-			back = back + step
+        front = back + 1
+        if (back + step) > size:
+            back = size
+        else:
+            back = back + step
 
-	guid = str(uuid.uuid4())
-	line = guid + '\t' + hash_md5.hexdigest() + '\t' + str(size) + '\t' + 'PROJECT-ID' + '\t' + 's3://' + bucketName + '/' + objectName
-	
-	fileName = "output/" + objectName + ".tsv"
-	print "putting the manifest file for object back into s3 without making file first"
-	bucket.put_object(Body = line.encode(), Key = fileName)
+    guid = str(uuid.uuid4())
+    line = (
+        guid
+        + "\t"
+        + hash_md5.hexdigest()
+        + "\t"
+        + str(size)
+        + "\t"
+        + "PROJECT-ID"
+        + "\t"
+        + "s3://"
+        + bucketName
+        + "/"
+        + objectName
+    )
+
+    fileName = "output/" + objectName + ".tsv"
+    print "putting the manifest file for object back into s3 without making file first"
+    bucket.put_object(Body=line.encode(), Key=fileName)
+
+    print line
+
+    return hash_md5.hexdigest()
 
 
-	print line
-
-	return hash_md5.hexdigest()
-
-if __name__ == '__main__':
-	args = parse_arguments()
-	md5 = md5ObjectSum(args.bucketName, args.objectName)
-	
+if __name__ == "__main__":
+    args = parse_arguments()
+    md5 = md5ObjectSum(args.bucketName, args.objectName)
